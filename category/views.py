13893bf28365befead81363d7c19adf905bb0cc9
g1rjeevan <jeevanrpro@gmail.com>
2018-08-20 15:27:56 +0530

    Minor Change

--- category/views.py
+++ category/views.py
@@ -51,21 +51,21 @@ class CategoryDetail(APIView):
             raise Http404
 
     def get(self, request, pk, format=None):
-        person = self.get_object(pk)
-        serializer = CategorySerializer(person)
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        person = self.get_object(pk)
-        serializer = CategorySerializer(person, data=request.data)
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        person = self.get_object(pk)
-        person.delete()
+        category = self.get_object(pk)
+        category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
